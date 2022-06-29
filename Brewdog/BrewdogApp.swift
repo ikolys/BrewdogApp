68a624e9d82625c142e4341df350423ca1df749f
Izabela Kołyszko <izabela.kolyszko@lhsystems.com>
2022-06-29 20:27:23 +0200

    Initial Commit

---
+++ Brewdog/BrewdogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewdogApp.swift
+//  Brewdog
+//
+//  Created by Izabela Kołyszko on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrewdogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
